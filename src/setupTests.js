a31623ac45a47e069160e77ac8b4e43f37e2bf4d
Carlos Reyes <reyesrico@hotmail.com>
2026-04-01 16:00:30 -0600

    fix(test): node20 compatibility for enzyme stack

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,7 +1,6 @@
-import { TextDecoder, TextEncoder } from 'util';
-
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+const { TextDecoder, TextEncoder } = require('util');
+const { ReadableStream, WritableStream, TransformStream } = require('stream/web');
+const { MessageChannel, MessagePort } = require('worker_threads');
 
 if (!global.TextEncoder) {
   global.TextEncoder = TextEncoder;
@@ -10,5 +9,28 @@ if (!global.TextEncoder) {
 if (!global.TextDecoder) {
   global.TextDecoder = TextDecoder;
 }
+
+if (!global.ReadableStream) {
+  global.ReadableStream = ReadableStream;
+}
+
+if (!global.WritableStream) {
+  global.WritableStream = WritableStream;
+}
+
+if (!global.TransformStream) {
+  global.TransformStream = TransformStream;
+}
+
+if (!global.MessageChannel) {
+  global.MessageChannel = MessageChannel;
+}
+
+if (!global.MessagePort) {
+  global.MessagePort = MessagePort;
+}
+
+const Enzyme = require('enzyme');
+const Adapter = require('enzyme-adapter-react-16');
 
 Enzyme.configure({ adapter: new Adapter() });
