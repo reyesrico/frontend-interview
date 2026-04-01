586a28fa138d4287e7bc1c576e755bd1fcc5009f
Carlos Reyes <reyesrico@hotmail.com>
2026-04-01 15:38:57 -0600

    chore(runtime): align frontend force branch to node20

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,14 @@
+import { TextDecoder, TextEncoder } from 'util';
+
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+
+if (!global.TextEncoder) {
+  global.TextEncoder = TextEncoder;
+}
+
+if (!global.TextDecoder) {
+  global.TextDecoder = TextDecoder;
+}
 
 Enzyme.configure({ adapter: new Adapter() });
